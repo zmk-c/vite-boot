6b159658d751afd7819be9b82d2a8dc75cdd886e
zhangmaokai <zmkfml@163.com>
2023-08-14 15:35:21 +0800

    提交格式化代码

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Author: zhangmaokai zmkfml@163.com
+ * @Date: 2023-08-10 15:48:12
+ * @LastEditors: zhangmaokai zmkfml@163.com
+ * @LastEditTime: 2023-08-14 15:01:58
+ * @FilePath: /vite-project/src/App.vue
+ * @Description: 
+-->
+<template>
+	<div>
+		<h1>我是主App</h1>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: zhangmaokai zmkfml@163.com
+ * @Date: 2023-08-10 15:48:12
+ * @LastEditors: zhangmaokai zmkfml@163.com
+ * @LastEditTime: 2023-08-14 11:27:50
+ * @FilePath: /vite-project/src/main.ts
+ * @Description: main.ts函数主入口
+ */
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
