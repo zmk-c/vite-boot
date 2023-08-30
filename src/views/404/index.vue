5793b8aece91e68f564f03be1a174b68fb2ee06a
zhangmaokai <zmkfml@163.com>
2023-08-31 01:30:43 +0800

    feat: 404NotFound页面

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -2,16 +2,37 @@
  * @Author: zhangmaokai zmkfml@163.com
  * @Date: 2023-08-16 08:55:00
  * @LastEditors: zhangmaokai zmkfml@163.com
- * @LastEditTime: 2023-08-16 08:55:04
+ * @LastEditTime: 2023-08-31 01:29:35
  * @FilePath: /vite-boot/src/views/404/index.vue
  * @Description: 
 -->
 <template>
-	<div>
-		<h1>404页面找不到了</h1>
+	<div class="notFound">
+		<img src="../../assets/images/error_images/404.png" />
 	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+let $router = useRouter();
+const goHome = () => {
+	$router.push('/home');
+};
 
-<style scoped></style>
+onMounted(() => {
+	setTimeout(() => {
+		goHome();
+	}, 1000);
+});
+</script>
+
+<style scoped lang="scss">
+.notFound {
+	img {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+}
+</style>
